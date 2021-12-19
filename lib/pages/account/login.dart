31e95b3cb547021b53fc495ba2a2708936f9b326
xieyimaple <1067545977@qq.com>
2021-12-19 20:19:08 +0800

    新增登录组件

---
+++ lib/pages/account/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
